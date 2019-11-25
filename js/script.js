8f407042db33155ad25b83b396690e19a2edfd99
Bruno Vinicius Souza de Sa <bruno.sa@opentech.local>
2019-11-25 11:16:10 -0300

    Front atualizado, 25/11/2019

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
-//POSICIONAMENTO SIDEBAR INÍCIO
-$( document ).ready(function() {
+//POSICIONAMENTO SIDEBAR - INÍCIO
+$(document).ready(function () {
     contentPosition('#sideContent');
     contentPosition('#formulario');
 
 });
 
-function contentPosition(customVar){
+function contentPosition(customVar) {
     container = $('#sidebar').outerHeight();
-    content   = $(customVar).outerHeight();
-    position  = (container-content)/2;
+    content = $(customVar).outerHeight();
+    position = (container - content) / 2;
 
     $(customVar).offset({
-        top : position
+        top: position
     });
 }
-//POSICIONAMENTO SIDEBAR FIM
 
-//MOSTRAR SENHA INÍCIO
+
+//MOSTRAR SENHA - INÍCIO
 function myFunction() {
-  var x = document.getElementById("password-field");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+    var x = document.getElementById("password-field");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
 }
-//MOSTRAR SENHA FIM
 
-//EFEITO SENHA ERRADA INÍCIO
 
-$(document).ready(function() {
-$("#login-btn").click(function() {
-var email = $("#Email").val();
-var password = $("#password-field").val();
-if (email == "bruno.sa@opentechgr.com.br" && password == "acertoumiseravi") {
-$("#message").html("Account Validated!!!");
-}
-else {
-$("#formulario").effect("shake");
-$("#message").html('***Invalid email or password***');
-}
+//EFEITO SENHA ERRADA - INÍCIO
+
+$(document).ready(function () {
+    $("#login-btn").click(function () {
+        var email = $("#Email").val();
+        var password = $("#password-field").val();
+        if (email == "bruno.sa@opentechgr.com.br" && password == "acertoumiseravi") {
+            $("#message").html("Ok");
+        } else {
+            $("#formulario").effect("shake");
+            $("#message").html('Login ou senha inválidos');
+        }
+    });
 });
-});
-//EFEITO SENHA ERRADA INÍCIO
+

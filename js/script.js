4e74384a87a2d8785ee106172ea844a05705b83d
Bruno Vinicius Souza de Sa <bruno.sa@opentech.local>
2019-11-25 10:51:03 -0300

    JS SHAKE

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+//POSICIONAMENTO SIDEBAR INÍCIO
 $( document ).ready(function() {
     contentPosition('#sideContent');
     contentPosition('#formulario');
-    myFunction();
+
 });
 
 function contentPosition(customVar){
@@ -13,8 +14,9 @@ function contentPosition(customVar){
         top : position
     });
 }
+//POSICIONAMENTO SIDEBAR FIM
 
-
+//MOSTRAR SENHA INÍCIO
 function myFunction() {
   var x = document.getElementById("password-field");
   if (x.type === "password") {
@@ -23,3 +25,21 @@ function myFunction() {
     x.type = "password";
   }
 }
+//MOSTRAR SENHA FIM
+
+//EFEITO SENHA ERRADA INÍCIO
+
+$(document).ready(function() {
+$("#login-btn").click(function() {
+var email = $("#Email").val();
+var password = $("#password-field").val();
+if (email == "bruno.sa@opentechgr.com.br" && password == "acertoumiseravi") {
+$("#message").html("Account Validated!!!");
+}
+else {
+$("#formulario").effect("shake");
+$("#message").html('***Invalid email or password***');
+}
+});
+});
+//EFEITO SENHA ERRADA INÍCIO
